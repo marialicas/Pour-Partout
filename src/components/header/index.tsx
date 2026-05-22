48bd80ef07d74b2a8817503bfae17673ce2adf83
marialicas <eialicag@gmail.com>
2026-05-22 19:48:50 -0300

    feat:header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,28 @@
 import "./style.css";
 import Logo from "../../assets/icons/marca.svg?react";
+import Bag from "../../assets/icons/bag.svg?react";
 
 function Header() {
   return (
-    <>
-      <Logo />
-    </>
+    <div className="box-shadow">
+      <header className="header">
+        <Logo />
+        <nav aria-label="Menu" className="menu">
+          <a className="page" href="">
+            Bolsas
+          </a>
+          <a className="page" href="">
+            Crie a Sua
+          </a>
+          <a className="page" href="">
+            Contatos
+          </a>
+          <a href="">
+            <Bag className="bag" />
+          </a>
+        </nav>
+      </header>
+    </div>
   );
 }
 
